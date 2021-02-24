fa5f873e292862e45140127880091c9b568a9b4b
John Constantine <28708924+nthequilibrium@users.noreply.github.com>
2021-02-24 22:58:07 +0530

    Testing EJS template

--- app.js
+++ app.js
@@ -5,14 +5,15 @@ app.set("view engine", "ejs"); // setting express view engine to use ejs
 
 app.get("/", (req, res) => {
     let today = new Date();
+    let daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     let currentDay = today.getDay();
-    let day = "";
+    let day = daysOfTheWeek[currentDay];
 
-    if (currentDay === 6 || currentDay === 0) {
-        day = "Weekend";
-    } else {
-        day = "Weekday";
-    }
+    // if (currentDay === 6 || currentDay === 0) {
+    //     day = "Weekend";
+    // } else {
+    //     day = "Weekday";
+    // }
 
     res.render("list", { kindOfDay: day });
 });
