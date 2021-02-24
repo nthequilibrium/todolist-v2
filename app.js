1404165d3b0e89432b08e0d763b9c96228808d93
John Constantine <28708924+nthequilibrium@users.noreply.github.com>
2021-02-24 18:26:38 +0530

    add views folder with ejs file and set up to use ejs in app.js (npm install ejs)

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+app.set("view engine", "ejs"); // setting express view engine to use ejs
+
 app.get("/", (req, res) => {
     res.send("Hello");
 });
