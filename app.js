c6649ad9b360533e397312e889869b0f016d561d
John Constantine <28708924+nthequilibrium@users.noreply.github.com>
2021-02-24 18:36:26 +0530

    Testing the Express - EJS setup

--- app.js
+++ app.js
@@ -4,7 +4,17 @@ const app = express();
 app.set("view engine", "ejs"); // setting express view engine to use ejs
 
 app.get("/", (req, res) => {
-    res.send("Hello");
+    let today = new Date();
+    let currentDay = today.getDay();
+    let day = "";
+
+    if (currentDay === 6 || currentDay === 0) {
+        day = "Weekend";
+    } else {
+        day = "Weekday";
+    }
+
+    res.render("list", { kindOfDay: day });
 });
 
 app.listen(3000, () => {
