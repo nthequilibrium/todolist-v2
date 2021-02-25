74c40f1e8f8dd22f6b8d220678254d61b92be5c0
John Constantine <28708924+nthequilibrium@users.noreply.github.com>
2021-02-25 12:57:15 +0530

    Update code to present a list and allow users to add items using form. Also introduce code to capture submitted item and update the list accordingly

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
 
+var items = ["Buy Food", "Cook Food", "Eat Food"];
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
 app.set("view engine", "ejs"); // setting express view engine to use ejs
 
 app.get("/", (req, res) => {
@@ -13,7 +18,13 @@ app.get("/", (req, res) => {
 
     let day = today.toLocaleDateString("EN-IN", options);
 
-    res.render("list", { kindOfDay: day });
+    res.render("list", { kindOfDay: day, newListItems: items });
+});
+
+app.post("/", (req, res) => {
+    let item = req.body.newItem;
+    items.push(item);
+    res.redirect("/");
 });
 
 app.listen(3000, () => {
