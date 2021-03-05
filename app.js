7078a441fd96b2591aaabfcdcf71430be712d19e
John Constantine <28708924+nthequilibrium@users.noreply.github.com>
2021-03-05 17:15:30 +0530

    Change credentials to deploy app

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(express.static("public"));
 app.set("view engine", "ejs"); // setting express view engine to use ejs
 
 // setting up database
-mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect("mongodb+srv://admin-john:t3sT@123@cluster0.luldr.mongodb.net/todolistDB", {useNewUrlParser: true, useUnifiedTopology: true});
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'Error connecting to database'));
